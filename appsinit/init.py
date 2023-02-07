a2433b3a8a3a05c94ded97101185e1a7d6209747
antonio waghtom <waghtompereira@gmail.com>
2023-02-06 22:30:02 -0300

    front janela ram

--- appsinit/init.py
+++ appsinit/init.py
@@ -8,15 +8,18 @@ class SubJanela(QMainWindow):
 
     def j_b(self):
         self.j_bateria.show()
+##----------------------------------------------------------------------
+# /home/waghtom/Downloads/CTRL-C/envs/QT5_hard/qt5_hard/appsinit/init.py (python
+from app_ram.fe2 import GUIFe2Ram
 
-
-class Janela_bateria(QMainWindow):
+class Janela_bateria(
+    GUIFe2Ram,
+    QMainWindow):
     
-
     def __init__(self):
         super(Janela_bateria, self).__init__()        
 
-        self.setGeometry(600, 150, 800, 400) #j-XY app-XY
+        self.setGeometry(600, 200, 500, 300) #j-XY app-XY
         self.setStyleSheet("background-color: #B0E0E6")#
         self.setWindowTitle("HARDWARE-RAM")
 
